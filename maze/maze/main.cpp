d8a6f08b105c25b7dd95daa3b8e95fe10f8e2288
weilaimg1314 <weilaimg1314@gmail.com>
2016-11-22 22:29:56 +0800

    添加初始化函数

--- maze/maze/main.cpp
+++ maze/maze/main.cpp
@@ -8,4 +8,34 @@
 //  迷宫问题求解...............
 //
 //
+#define OK 1
+#define ERROR 0
+#include <iostream>
+using namespace std;
+typedef int Status;
 
+char maze[100][100];
+
+
+
+Status Init_Maze(){
+    int m,n;
+    cout << "请输入迷宫的长度：";
+    cin >> m ;
+    cout << "请输入迷宫的宽度：";
+    cin >> n;
+    cout << "请输入一个长为" << m << "宽为" << n << "的，由0与1构成的迷宫"<< endl;
+    cout << "其中，0为迷宫中的通路，1为迷宫中的障碍"<< endl;
+    for (int i = 1 ; i <= n ; i++){
+        for (int j = 1 ; j <= m ; j++){
+            cin >> maze[i][j];
+        }
+    }
+    cout << "迷宫初始化完毕！"<< endl;
+    return OK;
+}
+
+
+int main(){
+    
+}
